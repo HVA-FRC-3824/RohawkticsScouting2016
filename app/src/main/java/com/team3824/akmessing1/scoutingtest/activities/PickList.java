8c0bc8750dbdb70945c5bb09b8ec670a52cc6ad4
amessing <amessing@utk.edu>
2015-11-09 12:48:34 -0500

    Added Pick List activity and fragments

---
+++ app/src/main/java/com/team3824/akmessing1/scoutingtest/activities/PickList.java
@@ -0,0 +1,34 @@
+package com.team3824.akmessing1.scoutingtest.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.team3824.akmessing1.scoutingtest.R;
+import com.team3824.akmessing1.scoutingtest.adapters.PickListFragmentPagerAdapter;
+import com.team3824.akmessing1.scoutingtest.adapters.PitScoutFragmentPagerAdapter;
+
+public class PickList extends AppCompatActivity {
+    final private String TAG = "PickList";
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private PickListFragmentPagerAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_list);
+
+        Bundle extras = getIntent().getExtras();
+
+        findViewById(android.R.id.content).setKeepScreenOn(true);
+        viewPager = (ViewPager) findViewById(R.id.pick_list_view_pager);
+        adapter = new PickListFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout = (TabLayout)findViewById(R.id.pick_list_tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
--- app/src/main/java/com/team3824/akmessing1/scoutingtest/activities/Settings.java
+++ app/src/main/java/com/team3824/akmessing1/scoutingtest/activities/Settings.java
@@ -124,66 +124,92 @@ public class Settings extends AppCompatActivity {
             prefEditor.commit();
             Button homeButton = (Button)findViewById(R.id.homeButton);
             homeButton.setVisibility(View.VISIBLE);
-            switch (type)
+            if(type.equals("Admin") || type.equals("Strategy") || type.equals("Drive Team"))
             {
-                case "Admin":
-                case "Strategy":
-                case "Drive Team":
-                    StatsDB statsDB = new StatsDB(this,eventId);
-                case "Pit Scout":
-                    final PitScoutDB pitScoutDB = new PitScoutDB(this,eventId);
+                final StatsDB statsDB = new StatsDB(this,eventId);
+                final PitScoutDB pitScoutDB = new PitScoutDB(this,eventId);
 
-                    // If there are no matches in the database pull from schedule the blue alliance
-                    if (pitScoutDB.getNumTeams() == 0) {
-                        Log.d(TAG, "Table empty");
-                        RequestQueue queue = Volley.newRequestQueue(this);
-                        String url = "http://www.thebluealliance.com/api/v2/event/" + eventId + "/teams?X-TBA-App-Id=amessing:scoutingTest:v1";
-                        Log.d(TAG, "url: " + url);
+                // If there are no matches in the database pull from schedule the blue alliance
+                if (pitScoutDB.getNumTeams() == 0) {
+                    Log.d(TAG, "Table empty");
+                    RequestQueue queue = Volley.newRequestQueue(this);
+                    String url = "http://www.thebluealliance.com/api/v2/event/" + eventId + "/teams?X-TBA-App-Id=amessing:scoutingTest:v1";
+                    Log.d(TAG, "url: " + url);
 
-                        //Request schedule
-                        JsonRequest jsonReq = new JsonUTF8Request(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-                            @Override
-                            public void onResponse(JSONArray response) {
-                                Log.d(TAG, "Team List received");
-                                pitScoutDB.createTeamList(response);
-                            }
-                        }, new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                //TODO: Allow team list building
-                                Log.d(TAG, "Error: " + error.getMessage());
-                                Toast.makeText(Settings.this, "Error: receiving team list", Toast.LENGTH_LONG);
-                            }
-                        });
-                        queue.add(jsonReq);
-                    }
-                default: // "Super Scout", "Match Scout"
-                    final ScheduleDB scheduleDB = new ScheduleDB(this, eventId);
-                    // If schedule is empty then try to get one from the blue alliance
-                    if (scheduleDB.getNumMatches() == 0) {
-                        Log.d(TAG, "Table empty");
-                        RequestQueue queue = Volley.newRequestQueue(this);
-                        String url = "http://www.thebluealliance.com/api/v2/event/" + eventId + "/matches?X-TBA-App-Id=amessing:scoutingTest:v2";
-                        Log.d(TAG, "url: " + url);
-                        JsonRequest jsonReq = new JsonUTF8Request(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-                            @Override
-                            public void onResponse(JSONArray response) {
-                                Log.d(TAG, "Schedule received");
-                                scheduleDB.createSchedule(response);
-                            }
-                        }, new Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                //TODO: Schedule builder
-                                Log.d(TAG, "Error: " + error.getMessage());
-                                Toast.makeText(Settings.this,"Error: receiving schedule", Toast.LENGTH_LONG);
-                            }
-                        });
-                        queue.add(jsonReq);
-                    }
-                    break;
+                    //Request schedule
+                    JsonRequest jsonReq = new JsonUTF8Request(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            Log.d(TAG, "Team List received");
+                            pitScoutDB.createTeamList(response);
+                            statsDB.createTeamList(response);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //TODO: Allow team list building
+                            Log.d(TAG, "Error: " + error.getMessage());
+                            Toast.makeText(Settings.this, "Error: receiving team list", Toast.LENGTH_LONG);
+                        }
+                    });
+                    queue.add(jsonReq);
+                }
             }
+            else if(type.equals("Pit Scout"))
+            {
+                final PitScoutDB pitScoutDB = new PitScoutDB(this,eventId);
+
+                // If there are no matches in the database pull from schedule the blue alliance
+                if (pitScoutDB.getNumTeams() == 0) {
+                    Log.d(TAG, "Table empty");
+                    RequestQueue queue = Volley.newRequestQueue(this);
+                    String url = "http://www.thebluealliance.com/api/v2/event/" + eventId + "/teams?X-TBA-App-Id=amessing:scoutingTest:v1";
+                    Log.d(TAG, "url: " + url);
+
+                    //Request schedule
+                    JsonRequest jsonReq = new JsonUTF8Request(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            Log.d(TAG, "Team List received");
+                            pitScoutDB.createTeamList(response);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //TODO: Allow team list building
+                            Log.d(TAG, "Error: " + error.getMessage());
+                            Toast.makeText(Settings.this, "Error: receiving team list", Toast.LENGTH_LONG);
+                        }
+                    });
+                    queue.add(jsonReq);
+                }
+            }
+
+             final ScheduleDB scheduleDB = new ScheduleDB(this, eventId);
+             // If schedule is empty then try to get one from the blue alliance
+             if (scheduleDB.getNumMatches() == 0) {
+                 Log.d(TAG, "Table empty");
+                 RequestQueue queue = Volley.newRequestQueue(this);
+                 String url = "http://www.thebluealliance.com/api/v2/event/" + eventId + "/matches?X-TBA-App-Id=amessing:scoutingTest:v2";
+                 Log.d(TAG, "url: " + url);
+                 JsonRequest jsonReq = new JsonUTF8Request(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                     @Override
+                     public void onResponse(JSONArray response) {
+                         Log.d(TAG, "Schedule received");
+                         scheduleDB.createSchedule(response);
+                     }
+                 }, new Response.ErrorListener() {
+
+                     @Override
+                     public void onErrorResponse(VolleyError error) {
+                         //TODO: Schedule builder
+                         Log.d(TAG, "Error: " + error.getMessage());
+                         Toast.makeText(Settings.this, "Error: receiving schedule", Toast.LENGTH_LONG);
+                     }
+                 });
+                 queue.add(jsonReq);
+
+             }
             Toast.makeText(this, "Saved", Toast.LENGTH_LONG);
 
         }
