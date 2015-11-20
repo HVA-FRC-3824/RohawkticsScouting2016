f05343c7f2f0d70cc4a98a6832d187b354199e92
amessing <amessing@utk.edu>
2015-11-20 10:17:00 -0500

    Changed which version of fragment is used. (Later version is needed for fragment manager, so all were changed for consistency)

--- app/src/main/java/com/team3824/akmessing1/scoutingapp/fragments/ScoutFragment.java
+++ app/src/main/java/com/team3824/akmessing1/scoutingapp/fragments/ScoutFragment.java
@@ -1,6 +1,6 @@
 package com.team3824.akmessing1.scoutingapp.fragments;
 
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/team3824/akmessing1/scoutingapp/fragments/TeamVisuals.java
+++ app/src/main/java/com/team3824/akmessing1/scoutingapp/fragments/TeamVisuals.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
